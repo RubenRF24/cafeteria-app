bc4864ab88fed4697af873313a9d13e04e8fe2c8
Ruben Robles <112518036+RubenRF24@users.noreply.github.com>
2025-04-27 16:17:05 -0500

    Actualizar configuracion de MySQLContainer con version especifica y credenciales

--- src/test/java/com/rubenrf/cafeteria_app/TestcontainersConfiguration.java
+++ src/test/java/com/rubenrf/cafeteria_app/TestcontainersConfiguration.java
@@ -12,7 +12,10 @@ class TestcontainersConfiguration {
 	@Bean
 	@ServiceConnection
 	MySQLContainer<?> mysqlContainer() {
-		return new MySQLContainer<>(DockerImageName.parse("mysql:latest"));
+		return new MySQLContainer<>(DockerImageName.parse("mysql:8.3.0"))
+				.withDatabaseName("db_cafeteria")
+				.withUsername("root")
+				.withPassword("root");
 	}
 
 }
