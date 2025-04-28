b96237e291fc082c39e0c777073b8ae348229e5e
Ruben Robles <112518036+RubenRF24@users.noreply.github.com>
2025-04-28 14:39:23 -0500

    Agregar configuracion de OpenAPI/Swagger para documentacion

---
+++ src/main/java/com/rubenrf/cafeteria_app/config/OpenAPIConfig.java
@@ -0,0 +1,22 @@
+package com.rubenrf.cafeteria_app.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class OpenAPIConfig {
+
+        @Bean
+        public OpenAPI cafeteriaServiceAPI() {
+                return new OpenAPI()
+                                .info(new Info().title("Cafeteria API")
+                                                .description("Este es la API REST para el servicio de Cafeteria")
+                                                .version("v.0.0.1").license(new License().name("Apache 2.0")));
+        }
+
+}
